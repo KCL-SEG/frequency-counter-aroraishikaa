d0041baa630d3d288134cf53c4d04019b885115d
Ishika Arora <ishika.arora@kcl.ac.uk>
2023-12-08 15:42:37 +0000

    Complete task

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,11 @@
-"""Frequencies function."""
-"""ENTER YOUR SOLUTION HERE!"""
-
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    for item in items:
+        # Convert item to string
+        item_str = str(item)
+        # Count the occurrences
+        if item_str in frequencies:
+            frequencies[item_str] += 1
+        else:
+            frequencies[item_str] = 1
     return frequencies
